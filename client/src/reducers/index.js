e1d3495a7e383797ac8ff24fd56b550d1bc69ace
georgievayo <yo.ana.georgieva@abv.bg>
2018-05-04 08:59:12 +0300

    Add modals actions and reducers.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import users from './users.reducer';
 import invitations from './invitations.reducer';
 import errors from './error.reducer';
+import modals from './modals.reducer';
 
 const reducer = combineReducers({
     users,
     invitations,
-    errors
+    errors,
+    modals
 });
 
 export default reducer;
