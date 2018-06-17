dc9f478c1179389b4a6870b498058be7767c5b11
georgievayo <yo.ana.georgieva@abv.bg>
2018-06-17 16:37:25 +0300

    Change api urls.

--- client/src/requesters/api.js
+++ client/src/requesters/api.js
@@ -1,20 +1,39 @@
+// const api = {
+//     LOGIN: 'http://localhost:3003/api/login',
+//     REGISTER: 'http://localhost:3003/api/register',
+//     USERS: 'http://localhost:3003/api/users',
+//     REQUESTS: 'http://localhost:3003/api/requests',
+//     BOOKS: 'http://localhost:3003/api/books',
+//     EVENTS: 'http://localhost:3003/api/events',
+//     GENRES: 'http://localhost:3003/api/genres',
+//     AUTHORS: 'http://localhost:3003/api/authors',    
+//     RECOMMENDED_BOOKS: 'http://localhost:3003/api/recommendedbooks',
+//     RECOMMENDED_EVENTS: 'http://localhost:3003/api/recommendedevents',
+//     LATEST_BOOKS: 'http://localhost:3003/api/latestbooks',
+//     LATEST_EVENTS: 'http://localhost:3003/api/latestevents',
+//     BOOKS_SEARCH: 'http://localhost:3003/api/books/search',
+//     EVENTS_SEARCH: 'http://localhost:3003/api/events/search',
+//     USERS_SEARCH: 'http://localhost:3003/api/users/search',  
+//     INFO: 'http://localhost:3003/api/info'  
+// };
+
 const api = {
-    LOGIN: 'http://localhost:3003/api/login',
-    REGISTER: 'http://localhost:3003/api/register',
-    USERS: 'http://localhost:3003/api/users',
-    REQUESTS: 'http://localhost:3003/api/requests',
-    BOOKS: 'http://localhost:3003/api/books',
-    EVENTS: 'http://localhost:3003/api/events',
-    GENRES: 'http://localhost:3003/api/genres',
-    AUTHORS: 'http://localhost:3003/api/authors',    
-    RECOMMENDED_BOOKS: 'http://localhost:3003/api/recommendedbooks',
-    RECOMMENDED_EVENTS: 'http://localhost:3003/api/recommendedevents',
-    LATEST_BOOKS: 'http://localhost:3003/api/latestbooks',
-    LATEST_EVENTS: 'http://localhost:3003/api/latestevents',
-    BOOKS_SEARCH: 'http://localhost:3003/api/books/search',
-    EVENTS_SEARCH: 'http://localhost:3003/api/events/search',
-    USERS_SEARCH: 'http://localhost:3003/api/users/search',  
-    INFO: 'http://localhost:3003/api/info'  
+    LOGIN: 'https://books-hub-api.herokuapp.com/api/login',
+    REGISTER: 'https://books-hub-api.herokuapp.com/api/register',
+    USERS: 'https://books-hub-api.herokuapp.com/api/users',
+    REQUESTS: 'https://books-hub-api.herokuapp.com/api/requests',
+    BOOKS: 'https://books-hub-api.herokuapp.com/api/books',
+    EVENTS: 'https://books-hub-api.herokuapp.com/api/events',
+    GENRES: 'https://books-hub-api.herokuapp.com/api/genres',
+    AUTHORS: 'https://books-hub-api.herokuapp.com/api/authors',    
+    RECOMMENDED_BOOKS: 'https://books-hub-api.herokuapp.com/api/recommendedbooks',
+    RECOMMENDED_EVENTS: 'https://books-hub-api.herokuapp.com/api/recommendedevents',
+    LATEST_BOOKS: 'https://books-hub-api.herokuapp.com/api/latestbooks',
+    LATEST_EVENTS: 'https://books-hub-api.herokuapp.com/api/latestevents',
+    BOOKS_SEARCH: 'https://books-hub-api.herokuapp.com/api/books/search',
+    EVENTS_SEARCH: 'https://books-hub-api.herokuapp.com/api/events/search',
+    USERS_SEARCH: 'https://books-hub-api.herokuapp.com/api/users/search',  
+    INFO: 'https://books-hub-api.herokuapp.com/api/info'  
 };
 
 module.exports = api;
